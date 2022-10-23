7bb5682fb2fc2ab73a60c501eaf46e5d13d32618
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2022-10-23 12:50:54 -0400

    smart css path, abort printing path property

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,14 @@ function stringifyEvent(e: any) {
   return JSON.stringify(
     obj,
     (k, v) => {
-      // if (v instanceof Node) return "-Node-";
-      if (v instanceof HTMLElement) {
-        return v.outerHTML.split(">")[0] + ">";
+      if (k === "path") {
+        // abort path property since it can be long
+        return
       }
+      if (v instanceof Node) return "-Node-";
+      // if (v instanceof HTMLElement) {
+      //   return v.outerHTML.split(">")[0] + ">";
+      // }
       if (v instanceof Window) return "-Window-";
       if (v instanceof Function) return "-Function-";
       return v;
@@ -21,6 +25,7 @@ function stringifyEvent(e: any) {
   );
 }
 
+// handles duplicates well
 function cssPath(el: any) {
   if (!(el instanceof Element)) return;
   var path = [];
@@ -38,7 +43,7 @@ function cssPath(el: any) {
       );
       selector += ":nth-child(" + nth + ")";
     }
-    console.log('selector', selector)
+    // console.log('selector', selector)
     if (!selector.startsWith("html")) {
       // skip html
       path.unshift(selector);
@@ -46,6 +51,115 @@ function cssPath(el: any) {
     el = el.parentNode;
   }
   return path.join(" > ");
+}
+
+// handles duplicates well
+function cssPathUsingClass(el: any) {
+  if (!(el instanceof Element)) return;
+  var path = [];
+  while (el.nodeType === Node.ELEMENT_NODE) {
+    var selector = el.nodeName.toLowerCase();
+    if (el.id) {
+      selector += "#" + el.id;
+    } else if ([].slice.call(el.classList).join('.')) {
+      selector += "." + [].slice.call(el.classList).join('.')
+    } else {
+      var sib = el,
+        nth = 1;
+      while (
+        sib.nodeType === Node.ELEMENT_NODE &&
+        (sib = sib.previousSibling) &&
+        nth++
+      );
+      selector += ":nth-child(" + nth + ")";
+    }
+    // console.log('selector', selector)
+    if (!selector.startsWith("html")) {
+      // skip html
+      path.unshift(selector);
+    }
+    el = el.parentNode;
+  }
+  return path.join(" > ");
+}
+
+function cssPathUsingClassAndIndex(el: any) {
+  if (!(el instanceof Element)) return;
+  var path = [];
+  while (el.nodeType === Node.ELEMENT_NODE) {
+    var selector = el.nodeName.toLowerCase();
+    const classList = [].slice.call(el.classList).join('.')
+    if (classList) {
+      selector += "." + classList
+    }
+    if (el.id) {
+      selector += "#" + el.id;
+    } else {
+      var sib = el,
+        nth = 1;
+      while (
+        sib.nodeType === Node.ELEMENT_NODE &&
+        (sib = sib.previousSibling) &&
+        nth++
+      );
+      selector += ":nth-child(" + nth + ")";
+    }
+    // console.log('selector', selector)
+    if (!selector.startsWith("html")) {
+      // skip html
+      path.unshift(selector);
+    }
+    el = el.parentNode;
+  }
+  return path.join(" > ");
+}
+
+// doesn't handle duplicates well
+function cssPathUsingClassAndID(el: any) {
+  if (!(el instanceof Element)) return;
+  var path = [];
+  while (el.nodeType === Node.ELEMENT_NODE) {
+    var selector = el.nodeName.toLowerCase();
+    if ([].slice.call(el.classList).join('.')) {
+      selector += "." + [].slice.call(el.classList).join('.')
+    }
+    if (el.id) {
+      selector += "#" + el.id;
+    }
+    // console.log('selector', selector)
+    if (!selector.startsWith("html")) {
+      // skip html
+      path.unshift(selector);
+    }
+    el = el.parentNode;
+  }
+  return path.join(" > ");
+}
+
+// doesn't handle duplicates well
+function altSelector(originalEl: any) {
+  var el = originalEl;
+  if (el instanceof Node) {
+    // Build the list of elements along the path
+    var elList = [];
+    do {
+      if (el instanceof Element) {
+        var classString = el.classList ? [].slice.call(el.classList).join('.') : '';
+        var elementName = (el.tagName ? el.tagName.toLowerCase() : '') + 
+            (classString ? '.' + classString : '') + 
+            (el.id ? '#' + el.id : '');
+        if (elementName) elList.unshift(elementName);
+      }
+      el = el.parentNode
+    } while (el != null);
+    // Get the stringified element object name
+    var objString = originalEl.toString().match(/\[object (\w+)\]/);
+    var elementType = objString ? objString[1] : originalEl.toString();
+    var cssString = elList.join(' > ');
+    // Return the CSS path as a string, prefixed with the element object name
+    // return cssString ? elementType + ': ' + cssString : elementType;
+    return cssString
+  }
 }
 
 export default function App() {
@@ -61,6 +175,12 @@ export default function App() {
     });
     console.log(event.target);
     console.log(cssPath(event.target));
+    // console.log(cssPathUsingClass(event.target));
+    const sel = cssPathUsingClassAndIndex(event.target)
+    console.log(sel);
+    console.log(sel?.replace(/(\..+?:)/gm, ':')); // remove classes (now same as cssPath)
+    // console.log(cssPathUsingClassAndID(event.target));
+    // console.log(altSelector(event.target));
   };
 
   useEffect(() => {
@@ -71,7 +191,7 @@ export default function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="App test hey">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <button
@@ -79,7 +199,14 @@ export default function App() {
           console.log("button clicked");
         }}
       >
-        Button
+        Button0
+      </button>
+      <button
+        onClick={() => {
+          console.log("button clicked");
+        }}
+      >
+        Button1
       </button>
       <div
         onClick={() => {
